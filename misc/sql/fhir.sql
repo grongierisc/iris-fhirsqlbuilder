49e6c1247e38f3c96f79fe36f2c5a43c0f3d0a22
grongierisc <guillaume.rongier@intersystems.com>
2023-01-31 17:39:25 +0100

    update sql

--- misc/sql/fhir.sql
+++ misc/sql/fhir.sql
@@ -15,9 +15,9 @@ FROM AA.Patient P
 Left join AB.DriverLicense DL on P.IdentifierValue = DL.Code;
 
 SELECT
-ID, DescriptionText, Key, LifecycleStatus, SubjectReference, $piece(GetEntitiesList(DescriptionText),'|',1) as entities
+ID, DescriptionText, Key, LifecycleStatus, SubjectReference, $piece(sqliknowparser(DescriptionText),'|',1) as entities
 FROM AA.Goal 
 union
 SELECT 
-ID, DescriptionText, Key, LifecycleStatus, SubjectReference, $piece(GetEntitiesList(DescriptionText),'|',2) as entities
+ID, DescriptionText, Key, LifecycleStatus, SubjectReference, $piece(sqliknowparser(DescriptionText),'|',2) as entities
 FROM AA.Goal
