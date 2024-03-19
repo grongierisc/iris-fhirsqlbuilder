e42322392b202a2044a52c5a3edd80d2be04d535
grongierisc <guillaume.rongier@intersystems.com>
2024-03-19 16:12:43 +0100

    Update pom.xml and add application.properties and ConfigDatabaseProperties.java

--- java/my-app/src/main/java/com/mycompany/app/App.java
+++ java/my-app/src/main/java/com/mycompany/app/App.java
@@ -1,40 +1,52 @@
 package com.mycompany.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 import java.sql.*;
 import com.intersystems.jdbc.*;
 
-/**
- * This a simple Java application
- * that aims to connect to an IRIS database
- * and perform some basic operations.
- */
-public class App 
-{
-    public static void main( String[] args ) throws Exception
-    {
-        // Create a variable for the connection string
-        String url = "jdbc:IRIS://localhost:33782/FHIRSERVER";
-        // Create a variable for the username
-        String username = "_SYSTEM";
-        // Create a variable for the password
-        String password = "SYS";
-        // Create a connection to the database
+@SpringBootApplication
+public class App {
 
-        Class.forName("com.intersystems.jdbc.IRISDriver").getDeclaredConstructor().newInstance();
-        IRISDataSource ds = new IRISDataSource();
-        ds.setURL(url);
-        ds.setUser(username);
-        ds.setPassword(password);
-        Connection connection = ds.getConnection();
-        // Create a statement
-        Statement stmt = connection.createStatement();
-        // Create a result set
-        ResultSet rs = stmt.executeQuery("SELECT * FROM AA.Patient");
-        // Iterate through the result set
-        while (rs.next()) {
-            System.out.println(rs.getString("NameFamily"));
-        }
+    @Autowired
+    private ConfigDatabaseProperties conf;
 
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            // Get the connection details from the application.properties file
+            String url = conf.getUrl();
+            String username = conf.getUsername();
+            String password = conf.getPassword();
+
+            // Create a connection to the database
+            Class.forName("com.intersystems.jdbc.IRISDriver").getDeclaredConstructor().newInstance();
+            IRISDataSource ds = new IRISDataSource();
+            ds.setURL(url);
+            ds.setUser(username);
+            ds.setPassword(password);
+            Connection connection = ds.getConnection();
+            // Create a statement
+            Statement stmt = connection.createStatement();
+            // Create a result set
+            ResultSet rs = stmt.executeQuery("SELECT * FROM AA.Patient");
+            // Iterate through the result set
+            while (rs.next()) {
+                System.out.println(rs.getString("NameFamily"));
+            }
+
+            // Close the resources
+            rs.close();
+            stmt.close();
+            connection.close();
+        };
     }
 }
